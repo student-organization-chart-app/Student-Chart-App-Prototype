f87ba9b80e37a253546dd5bda0ab4af174922190
Shankavi <101113678@georgebrown.ca>
2019-03-08 10:34:35 -0800

    update html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-let 
+var new subject=document.querySelector('#new-subject');
+var addTaskBtn = document.querySelector('#addTask');
+var todoUl= document.querySelector(".todo-list ul");
+var completeUl= doucment.querySelector("complete-list ul");
